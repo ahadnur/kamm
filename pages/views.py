5118077d05f36825d03249077c8d6461fa2a9390
Psy Coder <psycoder@lenovo>
2020-04-24 23:50:18 +0600

    first commit of kamm

---
+++ pages/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+def about_us(request):
+    return render(request, 'pages/about.html')
+
+def programs(request):
+    return render(request, 'pages/programs.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+def ch_autist(request):
+    return render(request, 'pages/ch_autist.html')
